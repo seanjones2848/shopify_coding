2f5eba6e2ca9262576be8034ec4fadad4a86a05c
Sean Jones <seanjones2848@gmail.com>
2023-03-23 16:20:16 -0700

    import and export works, one bug in list order

--- shopify_parser.py
+++ shopify_parser.py
@@ -1,18 +1,55 @@
 import csv
 
-products = {}
+repeat_column_keys = {"Handle"}
 
-with open('products_export_1.csv', newline='') as csvfile:
-    shopifyreader = csv.reader(csvfile, delimiter=',')
-    for idx, row in enumerate(shopifyreader):
-        if idx == 0:
-            keys = row
-        else:
-            if row[0] not in products:
-                products[row[0]] = {k:[v] for k,v in zip(keys, row)}
+def convert_csv_to_obj():
+    products = {}
+    with open('products_export_1.csv', newline='') as csvfile:
+        shopifyreader = csv.reader(csvfile, delimiter=',')
+        for idx, row in enumerate(shopifyreader):
+            if idx == 0:
+                keys = row
             else:
-                for i, key in enumerate(keys):
-                    if row[i] != "" and row[i] not in products[row[0]][key]:
-                        products[row[0]][key].append(row[i])
+                product = row[0]
+                if product not in products:
+                    products[product] = {k:[v] for k,v in zip(keys, row)}
+                else:
+                    for i, key in enumerate(keys):
+                        value = row[i]
+                        if value != "":
+                            if value in products[product][key] and len(products[product][key]) == 1:
+                                repeat_column_keys.add(key)
+                                if key == 'Type':
+                                    print(row)
+                            else:
+                                products[row[0]][key].append(row[i])
+    return products
 
-print(products)
+def convert_obj_to_csv(products):
+    keys = products[list(products.keys())[0]].keys()
+    with open("product_update.csv", "w", newline="") as csvfile:
+        shopifywriter = csv.DictWriter(csvfile, delimiter=",", fieldnames=keys)
+        shopifywriter.writeheader()
+        for data in products.values():
+            list_max = max([len([y for y in data[x]]) for x in data.keys()])
+            for i in range(list_max):
+                values = []
+                for k in keys:
+                    if len(data[k]) > i:
+                        values.append(data[k][i])
+                    else:
+                        if k in repeat_column_keys:
+                            values.append(data[k][0])
+                        else:
+                            values.append("")
+                shopifywriter.writerow({k:v for k,v in zip(keys, values)})
+
+
+
+# prod = convert_csv_to_obj()
+# prod["shaped-hand-soaps"]["Option1 Value"] = ["flerp", "plerp"]
+# print(prod["shaped-hand-soaps"])
+# print(repeat_column_keys)
+
+# convert_obj_to_csv(prod)
+
